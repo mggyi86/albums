54de0f87d0f03292253127d72c48655f0030092a
Aung Kyaw <aung.kyaw@miaki.co>
2017-12-13 00:28:55 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//Import a library to help create a component
+// import 'react-devtools';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+
+// AppRegistry.registerComponent('albums', () => App);
+
+// Create a component
+const App = () => (
+        <View>
+          <Header headerText={'Albums'} />
+          <AlbumList />
+        </View>
+);
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+// echo 256 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
+// echo 32768 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
+// echo 65536 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
+// watchman shutdown-server
+//adb shell input keyevent 82
---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = (props) => {
+    return (
+        <Card>
+          <CardSection>
+            <View></View>
+            <View>
+              <Text>{props.album.title}</Text>
+              <Text>{props.album.artist}</Text>
+            </View>           
+          </CardSection>
+        </Card>
+    );
+};
+
+export default AlbumDetail;
