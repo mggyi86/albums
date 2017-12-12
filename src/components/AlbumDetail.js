c6e75d238405de574f663bfe7d8118270ff60024
Aung Kyaw <aung.kyaw@miaki.co>
2017-12-13 02:29:40 +0630

    commit

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,20 +1,73 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
-    return (
-        <Card>
-          <CardSection>
-            <View></View>
-            <View>
-              <Text>{props.album.title}</Text>
-              <Text>{props.album.artist}</Text>
-            </View>           
-          </CardSection>
-        </Card>
-    );
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+  const { 
+    headerConentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    ImageStyle
+  } = styles;
+
+  return (
+      <Card>
+        <CardSection>
+          <View style={thumbnailContainerStyle}>
+            <Image
+             style={thumbnailStyle}
+             source={{ uri: thumbnail_image }}
+            />
+          </View>
+          <View style={headerConentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
+            <Text>{artist}</Text>
+          </View>           
+        </CardSection>
+
+        <CardSection >
+          <Image
+           style={ImageStyle}
+           source={{ uri: image }} 
+          />
+        </CardSection>
+
+        <CardSection >
+          <Button onPress={() => Linking.openURL(url)} >
+            Buy Now
+          </Button>
+        </CardSection>
+      </Card>
+  );
+};
+
+const styles = {
+  headerConentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  ImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
